b8bce9d1f761a4731b72abab569b2abecf04ee93
Your Name <your.email@example.com>
2025-11-08 22:10:31 +0530

    practice

--- 01-print.sh
+++ 01-print.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 echo "hello-world"
+#if you dont give space in between echo and " it shows command not found
--- 05-variables-pass.sh
+++ 05-variables-pass.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 echo "please enter pin num" 
+read -s PIN-NUMBER #herE pin number is variable
 
+echo "pin entered is : $PIN_NUMBER

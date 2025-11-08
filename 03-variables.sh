2fb14c07592b63a7928ed50e614ada5ca623d6c3
Your Name <your.email@example.com>
2025-11-08 21:35:12 +0530

    practice

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#variables =dont repeat yourself DRY
+# NO SPACE BEFORE OR AFTER EQUAL
+PERSON1="JOHN"
+PERSON2="bean"
+echo "$PERSON1:: hi how are you $PERSON1 "
+echo "$PERSON2: i am fine bro bea$PERSON2"
+echo "$PERSON1: how is your preparation? $PERSON1 "
+echo "$PERSON2: going well $PERSON2"

b6a63857a59a1c139618d8d38000d645b00681ab
Your Name <your.email@example.com>
2025-11-08 22:00:47 +0530

    practice

---
+++ 04-variables-arguments.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#arguments usage
+PERSON1=$1
+PERSON2=$2
+echo "$PERSON1:: hi how are you $PERSON1 "
+echo "$PERSON2: i am fine bro $PERSON2"
+echo "$PERSON1: how is your preparation? $PERSON1 "
+echo "$PERSON2: going well $PERSON2"

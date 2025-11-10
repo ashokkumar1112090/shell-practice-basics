1f5b3e12cf741c8076d928adc1a6625f12812911
Your Name <your.email@example.com>
2025-11-10 09:31:17 +0530

    practice

---
+++ 09-datatypes.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+NUMBER1=100
+NUMBER2=200
+ #THIS IS A STRING NOT NUMBER everything in shell considered as string
+NAME=devops  
+SUM=(($NUMBER1+$NUMBER2+$NAME)) #dont shows error because here name is not taken as number
+                               #not umber so value zeroo here 
+
+echo " sum is : $SUM"

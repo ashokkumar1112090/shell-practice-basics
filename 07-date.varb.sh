f934085c4afc3042098e22f95308e5d822d3e381
Your Name <your.email@example.com>
2025-11-10 08:25:37 +0530

    practice

--- 07-date.varb.sh
+++ 07-date.varb.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-DATE=$(date)
+#DATE=$(date)
 
-echo "Timestamp executed: $DATE"
+#echo "Timestamp executed: $DATE"
+
+START_TIME=$(date +%s)
+
+sleep 15
+
+END_TIME=$(date =%s)
+
+TOTAL_TIME=$(($END_TIME-$START_TIME))
+
+echo "print executed in : $TOTAL_TIME in seconds"

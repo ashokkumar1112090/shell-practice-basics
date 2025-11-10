cfb61fcffd9da6715682ab06797dccf7ddaef7f2
Your Name <your.email@example.com>
2025-11-10 09:05:02 +0530

    practice

--- 08-special.varb.sh
+++ 08-special.varb.sh
@@ -6,4 +6,9 @@ echo "All varb paassed to the script :$*"
 echo " script name : $0"
 echo "current directory : $PWD"
 echo " whoo is running the script : $USER"
+echo "home directory of user : $HOME "
+echo "PID of this script : $$"
+sleep 10 & #if we gave this command then how will you know then see next line
+echo "PID of last command in bg : $!" # ps -ef | grep sleep command  = ! 
+
   

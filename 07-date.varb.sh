4c9636a73872587efd0cc71787e3668cdb14a92f
Your Name <your.email@example.com>
2025-11-10 08:28:12 +0530

    practice

--- 07-date.varb.sh
+++ 07-date.varb.sh
@@ -9,6 +9,7 @@ sleep 15
 
 END_TIME=$(date =%s)
 
-TOTAL_TIME=$(($END_TIME-$START_TIME))
+TOTAL_TIME=$(($END_TIME-$START_TIME)) #dobule curly braces indicates arthimetic operation 
+                                      # if you dont apply it wont print anything
 
 echo "print executed in : $TOTAL_TIME in seconds"

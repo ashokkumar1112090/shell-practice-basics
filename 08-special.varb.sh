172a1d04b8741bb80c38c5963c7ca34f9b950eaa
Your Name <your.email@example.com>
2025-11-10 08:57:27 +0530

    practice

--- 08-special.varb.sh
+++ 08-special.varb.sh
@@ -5,4 +5,5 @@ echo "All varb paassed to the script :$*"
 # $ sh 08-special.varb.sh ashy viny ......it will print @ ashy * viny
 echo " script name : $0"
 echo "current directory : $PWD"
+echo " whoo is running the script : $USER"
   

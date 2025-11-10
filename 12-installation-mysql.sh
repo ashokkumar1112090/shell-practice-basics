45a4fc7aa2f5d7b0db6df96e1275cc486fe9256f
Your Name <your.email@example.com>
2025-11-10 15:09:56 +0530

    practice

---
+++ 12-installation-mysql.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+USERID=$(id -u)
+
+if [ $USERID ne 0 ]; then
+  echo "ERROR : please run this script with root previlege"
+fi
+
+dnf install mysql -y
+
+if  [ $? ne 0 ]; then
+  echo " ERROR: INSTALLING MYSQL IS FAILURE"
+else
+  echo "installing mysql is success"
+fi

a8ec35158db83d71f7532f880160066cf954a06b
Your Name <your.email@example.com>
2025-11-11 08:04:28 +0530

    practice

--- 14-colors.sh
+++ 14-colors.sh
@@ -5,5 +5,5 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-echo "$G Hello linux welcome to colors $N"  #if $N not given next line also fiollows green color so to stop it
-echo "check this line without clor or not"
+echo  -e "$G Hello linux welcome to colors $N"  #if $N not given next line also fiollows green color so to stop it
+echo "check this line without clor or not"  #if -e not given then not prints any color

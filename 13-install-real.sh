e70dc3afbef1f9e5be8296b690a3d5b2b728eacf
Your Name <your.email@example.com>
2025-11-11 08:23:24 +0530

    practice

---
+++ 13-install-real.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+  echo "ERROR : please run this script with root previlege"
+  exit 1
+fi
+
+VALIDATE(){     
+    if  [ $1 -ne 0 ]; then
+      echo " ERROR: INSTALLING $2 IS FAILURE"
+      exit 1
+   else
+      echo "installing $2 is success"
+   fi
+}
+
+dnf install mysql -y
+VALIDATE $? "mysql"
+
+dnf install nginx -y
+VALIDATE $? "nginx"
+
+dnf install python -y
+VALIDATE $? "python"
---
+++ 15-colors-install.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ $USERID -ne 0 ]; then
+  echo -e " $R ERROR  $N: please run this script with root previlege"
+  exit 1
+fi
+
+VALIDATE(){     
+    if  [ $1 -ne 0 ]; then
+      echo -e " INSTALLING $2 IS  $R FAILURE $N"
+      exit 1
+   else
+      echo -e "installing $2 is $G success $N"
+   fi
+}
+
+dnf list installed mysql  #installed if it not found
+if [ $? ne 0 ]; then
+    dnf install mysql -y
+    VALIDATE $? "mysql"
+else
+    echo -e "mysql already existed $G skipping $N"
+fi  
+
+
+
+dnf list installed nginx
+if [ $? ne 0 ]; then
+    dnf install nginx -y
+    VALIDATE $? "nginx"
+else
+    echo -e "nginx already existed $G skipping $N"
+fi
+
+
+
+dnf list installed python3
+if [ $? ne 0 ]; then
+    dnf install python3
+    VALIDATE $? "python3"
+else
+    echo -e "python3 already existed $G skipping $N"
+fi
+    

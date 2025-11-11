cd885a5ad96c4fd49ac058ea4f02fb6bb971c16e
Your Name <your.email@example.com>
2025-11-11 08:33:08 +0530

    practice

--- 15-colors-install.sh
+++ 15-colors-install.sh
@@ -30,7 +30,7 @@ fi
 
 
 dnf list installed nginx
-if [ $? ne 0 ]; then
+if [ $? -ne 0 ]; then
     dnf install nginx -y
     VALIDATE $? "nginx"
 else
@@ -40,7 +40,7 @@ fi
 
 
 dnf list installed python3
-if [ $? ne 0 ]; then
+if [ $? -ne 0 ]; then
     dnf install python3
     VALIDATE $? "python3"
 else

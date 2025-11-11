86f977ff18d66f72c5d22c2438166f1e8671159e
Your Name <your.email@example.com>
2025-11-11 08:00:43 +0530

    practice

---
+++ 14-colors.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+echo "$G Hello linux welcome to colors $N"  #if $N not given next line also fiollows green color so to stop it
+echo "check this line without clor or not"

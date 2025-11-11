c8aa29ca45df34d34653aec2b2c4bdfbfaa21dce
Your Name <your.email@example.com>
2025-11-11 12:18:46 +0530

    practice

--- 16-logs.sh
+++ 16-logs.sh
@@ -6,7 +6,7 @@ Y="\e[33m"
 N="\e[0m"
 LOGS_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" #means /var/log/shell-practice/16-logs.log
 
 mkdir -p $LOGS_FOLDER
 echo "script start executed at: $(date)" | tee -a $LOG_FILE  # echo printed one to APPEND in log file
